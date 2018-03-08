65b72c19bd1230b532e5318a244e488199e54fa2
Pablo Rengifo <prengifo@belatrixsf.com>
2018-03-08 17:55:45 -0500

    lecture 49

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Recipe } from './../recipe.model';
+import { Recipe } from '../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,8 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is a simply test', 
-    'https://images.food52.com/uskcNPt1UWi6IrxFVa66FC_IvgE=/753x502/71827747-aa62-4589-b238-2b9e76c29b5b--081709F_0449.JPG' )
+    new Recipe('A test Recipe', 'This is a simply test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg' )
   ];
   constructor() { }
 
